05efab88ceb4f983b4f340dce90ca7b143dcc879
Gurulakshmi-123 <gurulakshmi2897@gmail.com>
2022-08-16 22:23:32 +0530

    null and undefined values

---
+++ null.js
@@ -0,0 +1,12 @@
+var temp;
+if(temp === undefined)
+console.log("true");
+else
+console.log("false");
+
+
+var temp=['a','b','c'];
+if(temp[3] === undefined)
+console.log("true");
+else
+console.log("false");
